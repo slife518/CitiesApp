d64e58ecccc80042e1ed4b1c361f5eb69d607fed
JK <slife518@gmail.com>
2019-12-30 17:31:06 +0900

    ..

--- src/Cities/City.js
+++ src/Cities/City.js
@@ -15,17 +15,92 @@ import { colors } from '../theme'
 
 class City extends React.Component{
 
+    static navigationOptions = (props) =>{
+        const {city} = props.navigation.state.params
+        return{
+            title : city.city,
+            headerTitleStyle:{
+                color : 'white', 
+                fontSize : 20, 
+                fontWeight: '400',
+                
+            }
+        }
+
+    }
+    
+    static = {
+        name : '', 
+        info:''
+    }
+    onChangeText = (key, value) =>{
+        this.setState({
+            [key]:value
+        })
+    }
+
+    addLocation=()=>{
+        if(this.state.name==='' || this.state.info === '')return
+        const {city} = this.props.navigation.state.params
+        const location = {
+            name : this.state.name, 
+            info : this.state.info
+        }
+        this.props.screenProps.addLocation(location, city)
+        this.setState({name : '', info : ''})
+    }
+
+
     render(){
+        const {city} = this.props.navigation.state.params
+        console.log('props: ', this.props)
+
         return(
             <View style={{flex:1}}>
 
                 <ScrollView contentContainerStyle={[!city.locations.length && {
                     flex: 1
                 }]}>
-                    <View style={{styles}}>
+                    <View style={[styles.locationContainer, !city.locations.length&&{flex : 1, justifyContent : 'center'}]}>
+                        {
+                            !city.locations.length && <CenterMessage message = 'No locations for this city!'/>
+                        }
+                        {
+                            city.locations.map((location, index) =>(
+                                <View key = {index} style={styles.locationContainer}>
+                                    <Text style={styles.locationName}>{location.name}></Text>
+                                    <Text style={styles.locationInfo}>{location.info}></Text>
+                                </View>
+
+                            ))
+                        }
 
                     </View>
                 </ScrollView>
+
+                <TextInput
+                    onChangeText = {val => this.onChangeText('name', val)}
+                    placeholder = 'Location name'
+                    value= {this.state.name}
+                    style={styles.input}
+                    placeholderTextColor = 'white'
+                    />
+                    
+                <TextInput
+                    onChangeText = {val => this.onChangeText('info', val)}
+                    placeholder = 'Location info'
+                    value= {this.state.info}
+                    style={[styles.input, style.input2]}
+                    placeholderTextColor = 'white'
+                    />
+                <View style={styles.buttonContainer}>
+                    <TouchableOpacity onPress={this.addLocation}>
+                        <View style = {styles.button}>
+                            <Text style = {styles.buttonText}> Add Location</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+
             </View>
         )
     }
@@ -36,6 +111,49 @@ const styles = StyleSheet.create({
         flex : 1
     }, 
     locationContainer:{
-        padd 
+        padddingBottom: 104 
+    }, 
+    input:{
+        height: 50,
+        backgroundColor : colors.primary,
+        color : 'white',
+        paddingHorizontal : 8, 
+        position: 'absolute',
+        width:'100%', 
+        bottom : 104, 
+        left : 0
+    }, 
+    input2:{
+        bottom: 52
+    },
+    buttonContainer:{
+        position: 'absolute',
+        bottom:0, 
+        left: 0, 
+        width : '100%'
+    }, 
+    button:{
+        height : 50, 
+        backgroundColor : colors.primary,
+        justifyContent : 'center',
+        alignItems : 'center'
+    }, 
+    buttonText:{
+        color:'white'
+
+    }, 
+    locationContainer:{
+        padding: 10, 
+        borderBottomColor : colors.primary,
+        borderBottomWidth : 2
+
+    }, 
+    locationName:{
+        fontSize : 20
+    }, 
+    locationInfo:{
+        color : 'rgba(0,0,0,5)'
     }
 })
+
+export default City
